5b03fe1efade80c4ee03c7b9a0e7f76c98be7f93
petereliaskraft@gmail.com <petereliaskraft@gmail.com>
2020-10-15 10:34:12 -0500

    partition inserts

--- sql/load_procedures.sql
+++ sql/load_procedures.sql
@@ -1,6 +1,6 @@
 load classes build/DBOSProcedures.jar;
 CREATE PROCEDURE FROM CLASS dbos.procedures.InsertTask;
-CREATE PROCEDURE FROM CLASS dbos.procedures.InsertWorker;
+CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.InsertWorker;
 CREATE PROCEDURE FROM CLASS dbos.procedures.SelectTaskWorker;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectWorker;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectPartitionedTaskWorker;
