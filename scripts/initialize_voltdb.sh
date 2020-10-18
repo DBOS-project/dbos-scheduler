dd9a6b124f35dbcf7f72f8feb3040cb5570e98b1
Qian Li <liqian.cs@gmail.com>
2020-10-18 17:20:51 -0400

    Update init script to use shared enterprise version.

--- scripts/initialize_voltdb.sh
+++ scripts/initialize_voltdb.sh
@@ -5,15 +5,25 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 # Enter the root dir of the repo.
 cd ${SCRIPT_DIR}/../
 
-voltdb init -f --dir=/var/tmp
-voltdb start -B --ignore=thp --dir=/var/tmp
-sleep 5
-sqlcmd < sql/create_task_table.sql
-sqlcmd < sql/create_worker_table.sql
+# On supercloud, use the shared scripts.
+SHARED_DIR="/home/gridsan/qianl/DBOS_shared/shared_scripts/voltdb"
+bash ${SHARED_DIR}/init_start.sh
+
+# On local cluster, uncomment the following lines.
+# voltdb init -f --dir=/var/tmp
+# voltdb start -B --ignore=thp --dir=/var/tmp
+# sleep 5
+
+# On supercloud, use the enterprise version. On local cluster, change the path.
+VOLT_HOME="/home/gridsan/groups/DBOS/shared/VoltDB/voltdb-ent-9.3.2"
+VOLTDB_BIN="${VOLT_HOME}/bin"
+
+${VOLTDB_BIN}/sqlcmd < sql/create_task_table.sql
+${VOLTDB_BIN}/sqlcmd < sql/create_worker_table.sql
 
 # Create obj directory
 mkdir -p obj/
 
 javac -classpath "$VOLT_HOME/voltdb/*" -d obj sql/*.java 
 jar cvf build/DBOSProcedures.jar -C obj .   
-sqlcmd < sql/load_procedures.sql
+${VOLTDB_BIN}/sqlcmd < sql/load_procedures.sql
