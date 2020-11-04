15d1fb82387c0e51b5ef2ac381ea65b6139965aa
Qian Li <liqian.cs@gmail.com>
2020-11-04 16:08:02 -0500

    WIP. now can dispatch.

--- src/simulation/MockPollWorker.cc
+++ src/simulation/MockPollWorker.cc
@@ -16,21 +16,119 @@
 
 DbosStatus MockPollWorker::setup() {
   std::cout << "Setup worker " << workerId_ << std::endl;
+
+  // Start executors first, waiting for tasks.
+  for (int i = 0; i < numExecutors_; ++i) {
+    threads_.push_back(new std::thread(&MockPollWorker::execute, this));
+  }
+
+  // Start dispatch thread
+  threads_.push_back(new std::thread(&MockPollWorker::dispatch, this));
   return true;
 }
 
 DbosStatus MockPollWorker::teardown() {
   // Clean up data and threads.
   std::cout << "Stop worker " << workerId_ << std::endl;
+  stopDispatch_ = true;
+  // Wait until the queue is empty
+  while (taskQueue_.size()) {
+    std::cout << "Waiting to drain task queue\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+  // Stop all executors.
+  std::unique_lock<std::mutex> lock(lock_);
+  stop_ = true;
+  lock.unlock();
+  cv_.notify_all();
+
+  for (size_t i = 0; i < threads_.size(); ++i) {
+    threads_[i]->join();
+    delete threads_[i];
+  }
   return true;
 }
 
 void MockPollWorker::dispatch() {
   //TODO
+  std::cout << "A dispatcher for worker " << workerId_ << "\n";
+  // Create a local VoltDB client.
+  voltdb::Client voltdbClient =
+      VoltdbWorkerUtil::createVoltdbClient();
+  try {
+    voltdbClient.createConnection(dbAddr_);
+  } catch (std::exception& e) {
+    std::cerr << "An exception occured while connecting to VoltDB "
+              << std::endl;
+    // TODO: more robust error handling.
+    throw;
+  }
+
+  std::vector<voltdb::Parameter> parameterTypes(3);
+  parameterTypes[0] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
+  parameterTypes[1] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
+  parameterTypes[2] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
+
+  do {
+    DbosId taskId = -1;
+    // Select a task from DB.
+	  voltdb::Procedure procedure("WorkerSelectTask",
+	                              parameterTypes);
+	  voltdb::ParameterSet* params = procedure.params();
+	  params->addInt32(pkey_).addInt32(workerId_).addInt32(1);
+	  voltdb::InvocationResponse r = voltdbClient.invoke(procedure);
+	  if (r.failure()) {
+	    std::cout << "WorkerSelecTask procedure failed. "
+	              << r.toString() << std::endl;
+	    continue;
+	  }
+	  std::vector<voltdb::Table> results = r.results();
+    if (results.size() < 1) {
+      std::cout << "Failed to find a task for worker " << workerId_ <<".\n";
+      taskId = -1;
+    } else {
+      // TODO: support top-k.
+      voltdb::Row row = results[0].iterator().next();
+      taskId = row.getInt64(0);
+	  }
+
+    if (taskId >= 0) {
+      std::unique_lock<std::mutex> lock(lock_);
+      taskQueue_.push(taskId);
+      // Manual unlocking before notifying.
+      lock.unlock();
+      cv_.notify_one();
+      std::cout << "dispatch taskId " << taskId << "\n";
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  } while (!stopDispatch_);
+  std::cout << "Stopped dispatcher for worker " << workerId_ << "\n";
 }
 
 void MockPollWorker::execute() {
   // TODO
+  std::cout << "An executor for worker " << workerId_ << "\n";
+  std::unique_lock<std::mutex> lock(lock_);
+  // Wait for tasks
+  do {
+    // Wait until we have a task, or stop signal.
+    cv_.wait(lock, [this]{return (taskQueue_.size() || stop_); });
+
+    // Get one task
+    if (!stop_ && taskQueue_.size()) {
+      DbosId taskId = taskQueue_.front();
+      taskQueue_.pop();
+
+      // unlock the lock.
+      lock.unlock();
+
+      std::cout << "worker " << workerId_ << " process taskId " << taskId << std::endl;
+
+      // Re-acquire the lock.
+      lock.lock();
+    }
+  } while (!stop_);
+  std::cout << "Stopped executor for worker " << workerId_ << "\n";
 }
 
 
--- src/simulation/MockPollWorker.h
+++ src/simulation/MockPollWorker.h
@@ -4,6 +4,10 @@
 #include <atomic>
 #include <string>
 #include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 
 #include "simulation/VoltdbWorkerUtil.h"
 #include "voltdb-client-cpp/include/Client.h"
@@ -21,6 +25,7 @@ public:
   void dispatch();
 
   // Execute tasks. Potentially run in dedicated executor thread pool.
+  // Borrow ideas from: https://github.com/embeddedartistry/embedded-resources/blob/master/examples/cpp/dispatch.cpp
   void execute(); 
 
   // Setup the worker.
@@ -37,6 +42,11 @@ public:
 private:
   int pkey_;
   int numExecutors_;
+  std::mutex lock_;  // protect the access to shared taskQueue_
+  std::vector<std::thread*> threads_;  // including dispatch and executor threads
+  std::queue<DbosId> taskQueue_;  // queue of task Ids
+  std::condition_variable cv_;  // used to sync dispatch queue and executors
+  bool stopDispatch_ = false;
 };
 
 #endif  // #ifndef MOCK_POLL_WORKER_H
