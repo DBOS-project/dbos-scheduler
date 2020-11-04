54774ce72fbf025206f4ee640e79fee5aa6e4baa
Qian Li <liqian.cs@gmail.com>
2020-11-04 14:24:31 -0500

    WIP, add basic skeleton for mock worker

--- src/simulation/MockPollWorker.cc
+++ src/simulation/MockPollWorker.cc
@@ -12,115 +12,25 @@
 #include "voltdb-client-cpp/include/TableIterator.h"
 #include "voltdb-client-cpp/include/WireType.h"
 
-#include "simulation/SinglePartitionedFIFOTaskScheduler.h"
+#include "simulation/MockPollWorker.h"
 
-#define SUCCESS 0
-#define NOWORKER -2
-
-static std::atomic<uint32_t> taskindex;
-
-void SinglePartitionedFIFOTaskScheduler::truncateWorkerTable() {
-  std::vector<voltdb::Parameter> parameterTypes(0);
-  voltdb::Procedure procedure("TruncateWorkerTable", parameterTypes);
-  voltdb::ParameterSet* params = procedure.params();
-  voltdb::InvocationResponse r = client_->invoke(procedure);
-  if (r.failure()) {
-    std::cout << "TruncateWorkerTable procedure failed. " << r.toString();
-  }
-}
-
-void SinglePartitionedFIFOTaskScheduler::truncateTaskTable() {
-  std::vector<voltdb::Parameter> parameterTypes(0);
-  voltdb::Procedure procedure("TruncateTaskTable", parameterTypes);
-  voltdb::ParameterSet* params = procedure.params();
-  voltdb::InvocationResponse r = client_->invoke(procedure);
-  if (r.failure()) {
-    std::cout << "TruncateTaskTable procedure failed. " << r.toString();
-  }
-}
-
-DbosStatus SinglePartitionedFIFOTaskScheduler::insertWorker(DbosId workerID,
-                                                      int32_t capacity) {
-  std::vector<voltdb::Parameter> parameterTypes(3);
-  parameterTypes[0] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
-  parameterTypes[1] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
-  parameterTypes[2] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
-
-  voltdb::Procedure procedure("InsertWorker", parameterTypes);
-  voltdb::ParameterSet* params = procedure.params();
-  params->addInt32(workerID).addInt32(capacity).addInt32(workerID %
-                                                         partitions_);
-  voltdb::InvocationResponse r = client_->invoke(procedure);
-  if (r.failure()) {
-    std::cout << "InsertWorker procedure failed. " << r.toString();
-    return false;
-  }
+DbosStatus MockPollWorker::setup() {
+  std::cout << "Setup worker " << workerId_ << std::endl;
   return true;
 }
 
-DbosStatus SinglePartitionedFIFOTaskScheduler::selectTaskWorker(DbosId taskID) {
-  std::vector<voltdb::Parameter> parameterTypes(2);
-  parameterTypes[0] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
-  parameterTypes[1] = voltdb::Parameter(voltdb::WIRE_TYPE_INTEGER);
-  // Actual num partitions.
-  int activePartitions =
-      std::min(partitions_, numWorkers_);
-  int pkey = rand() % activePartitions;
-
-  // Try to find an available worker in the partition.
-	for (int count = 1; count <= activePartitions; ++count) {
-	  voltdb::Procedure procedure("SelectSinglePartitionedTaskWorker",
-	                              parameterTypes);
-	  voltdb::ParameterSet* params = procedure.params();
-	  params->addInt32(pkey).addInt32(taskID);
-	  voltdb::InvocationResponse r = client_->invoke(procedure);
-	  if (r.failure()) {
-	    std::cout << "SelectSinglePartitionedTaskWorker procedure failed. "
-	              << r.toString() << std::endl;
-	    return false;
-	  }
-	  std::vector<voltdb::Table> results = r.results();
-	  voltdb::Row row = results[0].iterator().next();
-	  int status = row.getInt64(0);
-	  if (status == SUCCESS) {
-	    return true;
-	  } else if (status == NOWORKER) { // implementing basic functionality first
-	  	// std::cout << "no worker in partition " << count << std::endl;
-        // TODO: If there is no worker, need to check other partitions for available workers.
-				// TODO: If there are no available workers, put task in a buffer
-    }
-    pkey = (pkey + count) % activePartitions;
-  }
-  std::cout << "went through " << activePartitions << " partitions" << std::endl;
-  return false;
-}
-
-DbosStatus SinglePartitionedFIFOTaskScheduler::setup() {
-  // Clean up data from previous run.
-  truncateWorkerTable();
-  truncateTaskTable();
-  DbosStatus ret;
-  std::cout << "Foo" << std::endl;
-  for (int i = 0; i < numWorkers_; ++i) {
-    ret = insertWorker(i, workerCapacity_);
-    if (!ret) { std::cout << "unable to add worker " << i << std::endl; return false; }
-  }
+DbosStatus MockPollWorker::teardown() {
+  // Clean up data and threads.
+  std::cout << "Stop worker " << workerId_ << std::endl;
   return true;
 }
 
-DbosStatus SinglePartitionedFIFOTaskScheduler::teardown() {
-  // Clean up data from previous run.
-  truncateWorkerTable();
-  truncateTaskTable();
-  return true;
+void MockPollWorker::dispatch() {
+  //TODO
 }
 
-DbosStatus SinglePartitionedFIFOTaskScheduler::schedule() {
-	//std::cout << "Foo" << std::endl;
-  for (int i = 0; i < numTasks_; ++i) {
-    int taskId = taskindex.fetch_add(1);
-    DbosStatus status = selectTaskWorker(taskId);
-  	assert(status == true);
-  }
-  return true;
+void MockPollWorker::execute() {
+  // TODO
 }
+
+
--- src/simulation/MockPollWorker.h
+++ src/simulation/MockPollWorker.h
@@ -1,60 +1,42 @@
-#ifndef SINGLE_PARTITIONED_FIFO_TASK_SCHEDULER_H
-#define SINGLE_PARTITIONED_FIFO_TASK_SCHEDULER_H
+#ifndef MOCK_POLL_WORKER_H
+#define MOCK_POLL_WORKER_H
 
 #include <atomic>
 #include <string>
 #include <vector>
 
-#include "simulation/VoltdbSchedulerUtil.h"
+#include "simulation/VoltdbWorkerUtil.h"
 #include "voltdb-client-cpp/include/Client.h"
 
-// Since voltdb::Client only has private constructor, we cannot create a private
-// member variable of it. Each thread needs to:
-// (1) call static function createVoltdbClient() to get a local VoltDB client.
-// (2) pass the pointer to construct PartitionedFIFOScheduler.
-class SinglePartitionedFIFOTaskScheduler : public VoltdbSchedulerUtil {
+class MockPollWorker : public VoltdbWorkerUtil {
 public:
-  SinglePartitionedFIFOTaskScheduler(voltdb::Client* client, std::string dbAddr,
-                               int partitions, int numTasks, int workerCapacity,
-                               int numWorkers, float probMultiTx)
-      : VoltdbSchedulerUtil(client, dbAddr),
-        partitions_(partitions),
-        numTasks_(numTasks),
-        workerCapacity_(workerCapacity),
-        numWorkers_(numWorkers),
-        probMultiTx_(probMultiTx){};
+  MockPollWorker(int workerId, int pkey, std::string dbAddr,
+                 int numExecutors)
+      : VoltdbWorkerUtil(workerId, dbAddr),
+        pkey_(pkey),
+        numExecutors_(numExecutors) {};
 
-  // Truncate the worker table;
-  void truncateWorkerTable();
+  // Dispatch tasks that are assigned to this worker.
+  // Potentially run in a dedicated dispatch thread.
+  void dispatch();
 
-  // Truncate the worker table;
-  void truncateTaskTable();
+  // Execute tasks. Potentially run in dedicated executor thread pool.
+  void execute(); 
 
-  // Insert a worker into the worker table.
-  DbosStatus insertWorker(DbosId workerID, DbosId capacity);
-
-  // Select a worker for a task and update worker capacity and task workerid.
-  DbosStatus selectTaskWorker(DbosId taskID);
-
-  // Setup the database.
+  // Setup the worker.
+  // E.g., setup dispatch thread, and multiple executor threads.
   DbosStatus setup();
 
-  // Tear down the database after benchmarking.
+  // Stop the worker (all executor and dispatch threads) and free up resources.
   DbosStatus teardown();
 
-  // Perform a scheduling act.
-  DbosStatus schedule();
-
   // Destructor
-  ~SinglePartitionedFIFOTaskScheduler() { /* placeholder for now. */
+  ~MockPollWorker() { /* placeholder for now. */
   }
 
 private:
-  int partitions_;
-  int numTasks_;
-  int workerCapacity_;
-  int numWorkers_;
-  float probMultiTx_;
+  int pkey_;
+  int numExecutors_;
 };
 
-#endif  // #ifndef SINGLE_PARTITIONED_FIFO_TASK_SCHEDULER_H
+#endif  // #ifndef MOCK_POLL_WORKER_H
