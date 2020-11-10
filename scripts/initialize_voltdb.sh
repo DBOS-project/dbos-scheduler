2e14f8cef16ea6b6f888fb11a99d1075928e07ce
Qian Li <liqian.cs@gmail.com>
2020-11-09 19:15:01 -0500

    Add script to update procedure without restarting DB.

--- scripts/initialize_voltdb.sh
+++ scripts/initialize_voltdb.sh
@@ -18,12 +18,10 @@ bash ${SHARED_DIR}/init_start.sh
 VOLT_HOME="/home/gridsan/groups/DBOS/shared/VoltDB/voltdb-ent-9.3.2"
 VOLTDB_BIN="${VOLT_HOME}/bin"
 
+# Create tables.
 ${VOLTDB_BIN}/sqlcmd < sql/create_task_table.sql
 ${VOLTDB_BIN}/sqlcmd < sql/create_worker_table.sql
 
-# Create obj directory
-mkdir -p obj/
+# Load stored procedures
+bash ${SCRIPT_DIR}/update_voltdb_procedures.sh
 
-javac -classpath "$VOLT_HOME/voltdb/*" -d obj sql/*.java 
-jar cvf build/DBOSProcedures.jar -C obj .   
-${VOLTDB_BIN}/sqlcmd < sql/load_procedures.sql
--- sql/load_procedures.sql
+++ sql/load_procedures.sql
@@ -1,16 +1,46 @@
 load classes build/DBOSProcedures.jar;
+
+DROP PROCEDURE InsertTask IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.InsertTask;
+
+DROP PROCEDURE InsertWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey PARAMETER 2 FROM CLASS dbos.procedures.InsertWorker;
+
+DROP PROCEDURE InsertSparkWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey PARAMETER 3 FROM CLASS dbos.procedures.InsertSparkWorker;
+
+DROP PROCEDURE SelectTaskWorker IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.SelectTaskWorker;
+
+DROP PROCEDURE SelectWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectWorker;
+
+DROP PROCEDURE SelectPartitionedTaskWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectPartitionedTaskWorker;
+
+DROP PROCEDURE SelectSinglePartitionedTaskWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectSinglePartitionedTaskWorker;
+
+DROP PROCEDURE SelectSparkWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Worker COLUMN PKey FROM CLASS dbos.procedures.SelectSparkWorker;
+
+DROP PROCEDURE TruncateTaskTable IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.TruncateTaskTable;
+
+DROP PROCEDURE TruncateWorkerTable IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.TruncateWorkerTable;
+
+DROP PROCEDURE FinishWorkerTask IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.FinishWorkerTask;
+
+DROP PROCEDURE ScanPartitionedTaskWorker IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Task COLUMN PKey FROM CLASS dbos.procedures.ScanPartitionedTaskWorker;
+
+DROP PROCEDURE ScanTaskWorker IF EXISTS;
 CREATE PROCEDURE FROM CLASS dbos.procedures.ScanTaskWorker;
+
+DROP PROCEDURE WorkerSelectTask IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Task COLUMN PKey FROM CLASS dbos.procedures.WorkerSelectTask;
+
+DROP PROCEDURE WorkerUpdateTask IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE Task COLUMN PKey FROM CLASS dbos.procedures.WorkerUpdateTask;
