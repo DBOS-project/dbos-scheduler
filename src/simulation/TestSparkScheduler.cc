bdb028098ac18610f7e79b15a843579aeb3e70fe
petereliaskraft@gmail.com <petereliaskraft@gmail.com>
2020-12-11 12:52:09 -0600

    fix spark scheduler public/private split

--- src/simulation/TestSparkScheduler.cc
+++ src/simulation/TestSparkScheduler.cc
@@ -13,51 +13,28 @@ int main(int argc, char** argv) {
   SparkScheduler scheduler(&voltdbClient, "localhost", 1, 1, 2);
 
   // Insert then Select a worker.
-  scheduler.truncateWorkerTable();
   DbosStatus ret = scheduler.setup();
   assert(ret);
   DbosId workerId;
 
-  workerId = scheduler.selectWorker(0);
-  assert(workerId != -1);
-  ret = scheduler.finishTask(voltdbClient, 0, workerId);
-
-  workerId = scheduler.selectWorker(1);
-  assert(workerId != -1);
-  ret = scheduler.finishTask(voltdbClient, 0, workerId);
-
-  workerId = scheduler.selectWorker(0);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(0);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(0);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(1);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(0);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(1);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
-  workerId = scheduler.selectWorker(1);
-  assert(workerId != -1);
-  scheduler.finishTask(voltdbClient, -1, workerId);
-
   Task* task = new Task;
   task->execTime = 1000;
+
   task->requirement = 1;
   scheduler.schedule(task);
+
+  task->requirement = 0;
+  scheduler.schedule(task);
+
+  task->requirement = 1;
+  scheduler.schedule(task);
+
+  task->requirement = 0;
+  scheduler.schedule(task);
+
+  task->requirement = 0;
+  scheduler.schedule(task);
+
   delete task;
 
   scheduler.teardown();
